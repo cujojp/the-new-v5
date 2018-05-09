d0aaa55ca25e07dac91520d58bb9b67c09cfc960
cujojp <info@cujo.jp>
2018-05-08 20:30:03 -0700

    Adding prod build step

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -14,6 +14,7 @@ module.exports = options => {
     entry: './src/js/index.js',
     output: {
       filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
     },
     module: {
       loaders: [
